47b51a620189cdf687e6fc5963e01b39be927379
WSL043 <wangsr043@gmail.com>
2026-08-30 20:55:27 +0800

    Release DSH-Portable 0.5.4

--- launcher/windows/DSH-Portable.cs
+++ launcher/windows/DSH-Portable.cs
@@ -25,8 +25,8 @@ using Microsoft.Web.WebView2.WinForms;
 [assembly: AssemblyCompany("WSL043")]
 [assembly: AssemblyProduct("DeepSeek-Herness")]
 [assembly: AssemblyCopyright("Copyright © WSL043 2026")]
-[assembly: AssemblyVersion("0.5.3.65534")]
-[assembly: AssemblyFileVersion("0.5.3.65534")]
+[assembly: AssemblyVersion("0.5.4.65534")]
+[assembly: AssemblyFileVersion("0.5.4.65534")]
 
 namespace DshPortable
 {
